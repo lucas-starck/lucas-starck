bb2e3ec5c77853269774311b7b70473c07c42213
lucas-starck <lucas.a.starck@gmail.com>
2024-02-17 22:59:34 +0000

    PINN_NN_Linear formatted.

--- Iss_FYP/PINN_Linear/PINN_NN_Linear.py
+++ Iss_FYP/PINN_Linear/PINN_NN_Linear.py
@@ -1,4 +1,4 @@
-#%%
+#%%########################################################################################################
 #Imports
 import numpy as np
 import tensorflow as tf
@@ -7,132 +7,103 @@ import pandas as pd
 import matplotlib.pyplot as plt
 tf.config.run_functions_eagerly(True)
 
-#%%
-#Importing data from Excel
+
+
+
+#%%########################################################################################################
+# Importing data from Excel
 df = pd.read_excel('Abaqus_data.xlsx', 0, header=0)
 
-
-#Input data
-N= 864  #no. of elements in each specimen - i.e. batch size
-Exx_data = df.iloc[:, 1].values.astype('float32') # Exx size: (9504,1)
-Eyy_data = df.iloc[:, 3].values.astype('float32') # Eyy size: (9504,1)
-Exy_data = df.iloc[:, 5].values.astype('float32') # Exy size: (9504,1)
+# Extract input data
+N = 864  # no. of elements in each specimen - i.e. batch size
+Exx_data = df.iloc[:, 1].values.astype('float32') # Exx size: (8640,1)
+Eyy_data = df.iloc[:, 3].values.astype('float32') # Eyy size: (8640,1)
+Exy_data = df.iloc[:, 5].values.astype('float32') # Exy size: (8640,1)
+element_volume = df.iloc[:, 8].values.astype('float32') # Volume of each element for volume integral size: (8640,1)
 P_data = df.iloc[0:10, 12].astype('float32')   # P size: (11,1)
 Ux_data = df.iloc[0:10, 11].astype('float32')  # Ux size: (11,1)
-element_volume = df.iloc[:, 8].values.astype('float32') # Volume of each element for volume integral size: (9504,1)
 
-standardisation_params = tf.Variable(tf.zeros((2,4)), trainable=False)
-         
-#Combining the features into one array
-inputs = tf.stack([Exx_data, Eyy_data, Exy_data, element_volume], axis=1) #size: (9504, 3)
+# Combining features into one array
+inputs = tf.stack([Exx_data, Eyy_data, Exy_data, element_volume], axis=1) # size: (8640, 4)
 
-#calculating external work
-external_work =0.5* (P_data * Ux_data) # gives a vector of external work values at each time step/batch size: (11, 1)
+# Extracting scaling values to scale between (0,1)
+#   Note: we are scaling by the max/min value of all 10 batches, and not of each batch
+max_inputs = tf.reduce_max(inputs, axis=0)
+min_inputs = tf.reduce_min(inputs, axis=0)
 
-print(external_work)
+# Scaling inputs
+scaled_inputs = (inputs-min_inputs)/(max_inputs-min_inputs)
+normalisation_params = tf.Variable(tf.zeros((2,4)), trainable=False)
+normalisation_params.assign_add([max_inputs, min_inputs])
+print('Shape of normalisation parameters: ', normalisation_params.shape)
+print('Normalisation parameter values: ', normalisation_params.numpy())
 
+# Calculating external work
+#   This gives a vector of external work values at each time step/batch size: (11, 1)
+external_work = 0.5 * (P_data * Ux_data)
+print('External work: ', external_work)
+
+
+
+
+#%%########################################################################################################
 #Designing custom loss function
 
-#%%
-def custom_loss_with_params(external_work, standardisation_params):
-    def custom_loss(inputs, L):   
+# Wrapper function - lets you input global variables into loss function
+def custom_loss_with_params(external_work, normalisation_params): 
 
-        #unscaling the inputs
-        
-        max_inputs, min_inputs = tf.split(standardisation_params, 2)
+    # Loss function
+    def custom_loss(inputs_scaled, L):   
+            
+        # Initialise loss function components
+        strain_energy = 0.
+        n = float(len(inputs_scaled)) # no. elements
 
-        reversed_inputs = inputs * (max_inputs - min_inputs) + min_inputs
+        # Un-scaling inputs for physical calculations
+        max_inputs, min_inputs = tf.split(normalisation_params, 2)
+        inputs_unscaled = inputs_scaled * (max_inputs - min_inputs) + min_inputs
 
-        #undoing the inputs normalisation
-
-        '''
-        bn_layer = None
-        for layer in model.layers:
-            if isinstance(layer, tf.keras.layers.BatchNormalization):
-                bn_layer = layer
-                break
-
-        if bn_layer:
-            # Get the learned parameters (gamma and beta) and running averages
-            gamma, beta = bn_layer.get_weights()[:2]
-            mean, variance = bn_layer.moving_mean, bn_layer.moving_variance
-
-            # Unscale the inputs
-            epsilon = bn_layer.epsilon
-            unscaled_inputs = gamma * (inputs - beta) / tf.sqrt(variance + epsilon) + mean
-        
-        else:
-
-            print("BatchNormalization layer not found in the model.")
-        '''
-        array_inputs=np.array(reversed_inputs)
-        
+        # Separating inputs into variables
+        array_inputs=np.array(inputs_unscaled)
         Exx = array_inputs[:,0]
         Eyy = array_inputs[:,1]
         Exy = array_inputs[:,2]
         vol = array_inputs[:,3] 
-        
-        vol_tf=tf.convert_to_tensor(vol, dtype=tf.float32)
-        #epsilon is a combination of Exx, Eyy and Exy features
+        vol_tf = tf.convert_to_tensor(vol, dtype=tf.float32)
+
+        # Calculate epsilon and its transpose
         epsilon = tf.stack([Exx, Eyy, Exy], axis=1) # size: (N, 3)
-        epsilon = tf.reshape(epsilon , (epsilon.shape[0],3,1)) #sise:
-        #print('epsilon:', epsilon)
-        
-        # generate epsilonT
+        epsilon = tf.reshape(epsilon , (epsilon.shape[0],3,1))
         epsilonT = tf.transpose(epsilon,perm=[0,2,1]) # size: (3, N)
-        #print('EpsilonT:',epsilonT)
-        
-        n=864. #no. of elements in the strain field
-    
-        #initialise loss function components
-        strain_energy= 0.
-       
 
-        max_values = tf.reduce_max(L, axis=-1, keepdims=True)
-
-         
-        # Before passing the training data, L is size (none,9). But during training, L is size (N,9). 
-        '''
-        if L.shape[0]!=L.shape[0]:
-            L_matrix = tf.reshape(L,(1,3,3)) #output of Neural network such that C=LL^T - size should be a (N,3,3)
-        else: 
-            L_matrix = tf.reshape(L,(N,3,3)) #output of Neural network such that C=LL^T - size should be a (N,3,3)
-        #print(L_matrix.shape)
-        '''
-    
-        # L transpose
+        # Calculate L transpose from input L
         LT = tf.transpose(L,perm=[0,2,1])
-        #print(L_matrixT.shape)
         
-        # Calculate C
+        # Calculate C = L*LT
         C = tf.matmul(L, LT) # size: (N,3,3) 
-        #print('C', C)
-        #print('C shape', C.shape)
-    
-        #External work seems to be a tf. tensor and strain energy is a Keras tensor - not sure if this matters?
-    
-        # Calculation of strain energy - matmul functions perform the matrix multiplication E_T xCx E, dot multiply by element volume vector, outputting a scalar
-        temp1 = tf.matmul(epsilonT, tf.matmul(C, epsilon))
-        #print('temp1', temp1)
-        strain_energy = 0.5 * tf.tensordot(vol_tf , temp1 ,1)/n # do i need to times this by a half? # size is a scalar
-        print('strain energy value:', strain_energy)
-        
-        difference = tf.abs(1-(strain_energy/external_work[batch_no]))# calculates the squared difference between virtual work at the current batch and the strain energy - this may not be right
 
-       
-        #print('external work:',external_work.shape)
-        print('external work value:', external_work[batch_no]) # doesn't seem to be outputting the correct value. 
-        
-        #mean_squared_difference = tf.reduce_mean(squared_difference, axis=-1) #Averages squared difference across each batch? Not sure this is necessary actually
-        
-        #rmse = tf.sqrt(squared_difference)
-        print('difference RESULT: ',difference)
-        return difference
+        # Calculate strain energy: 
+        #   Matrix multiply E_T * C * E, and dot multiply by element volume, outputting a scalar
+        strain_energy = 0.5 * tf.tensordot(vol_tf , tf.matmul(epsilonT, tf.matmul(C, epsilon) ) ,1) / n # scalar value
+        print('Internal work (strain energy):   ', strain_energy[0][0].numpy())
+        print('External work (F x d):           ', external_work[batch_no]) 
+            
+        # External work seems to be a tf. tensor and strain energy is a Keras tensor - not sure if this matters?
+    
+        # Calculate Loss: 
+        #   Difference between internal (strain energy) and external work (F*d)
+        Energy_difference = tf.abs((external_work[batch_no])-((strain_energy)))
+        print('Energy difference:               ',Energy_difference[0][0].numpy())
+        return Energy_difference
 
     return custom_loss
 
 
-#%%
+
+
+#%%########################################################################################################
+# Creating ML Model
+
 def Create_Model(activ, Hidden_layers, node_num1, node_num2):
     """
     Creates a Keras sequential model with the specified number of hidden layers and nodes, 
@@ -173,90 +144,97 @@ def Create_Model(activ, Hidden_layers, node_num1, node_num2):
     return model
 
 
-#%%
 #Calling create model amd compiling model with optimizer
 
-model = Create_Model('linear',3,40,40)
+model = Create_Model(activ='linear', Hidden_layers=2, node_num1=20, node_num2=20)
 model.summary()
 model.compile(optimizer = tf.keras.optimizers.legacy.Adam(learning_rate = 0.001), 
-          loss = custom_loss_with_params(external_work=external_work, standardisation_params=standardisation_params)) #how do i make a custom loss function here?
+          loss = custom_loss_with_params(external_work = external_work, 
+                                         normalisation_params = normalisation_params))
 
 
 
-#%%
-#Train the model - what do i put in the place of train labels?
 
-'''
-history = model.fit(inputs, inputs, 
-                    batch_size = N,
-                    shuffle=False,
-                    epochs = 100, 
-                    verbose = 1) # haven't done validation data split yet
-#validation_data = (valid_values, valid_labels),
-'''
+#%%########################################################################################################
+# Train ML Model manually using train_on_batch
 
-#training using train_on_batch
-losses = []
-batch_size = N
-epochs = 50
+# Initialise vars
+loss_list = []
+batch_size = N # Train on entire specimen element set (864)
+batches_per_epoch = scaled_inputs.shape[0] // batch_size # Number of time steps / data sets for that specimen
+N_epochs = 30
 
-# Initialize max and min values
-#max_values = np.zeros((9,))
-#min_values = np.zeros((9,))
+# Iterate over each Epoch
+for epoch in range(N_epochs):
 
-for epoch in range(epochs):
+    total_loss = 0.0 # sum of loss of all batches in this epoch
+    print('Epoch number: ', epoch)
 
-    total_loss = 0.0
-    batches_per_epoch = inputs.shape[0] // batch_size
-    print('epoch number', epoch)
-    custom_loss_calls = tf.Variable(0, trainable=False, dtype=tf.int32) # intialising counter for number of times loss function is called
-    
-    for i in range(0, len(inputs), batch_size):
+    # Initialise counter to track the batch number. Note: it gets updated during the run of a loss function
+    custom_loss_calls = tf.Variable(0, trainable=False, dtype=tf.int32) 
+
+    # Iterate over each Batch
+    for batch_idx in range(0, len(scaled_inputs), batch_size): #(0, 864x10, 864)
+
         batch_no = custom_loss_calls.numpy()
-        print('batch number',batch_no)
-    
-        x_batch = inputs[i:i+batch_size]
-        
-        #performing feature scaling per batch instead of on whole dataset
-        max_x_batch = tf.reduce_max(x_batch, axis=0)
-        min_x_batch = tf.reduce_min(x_batch, axis=0)
-        
-        standardisation_params.assign_add([max_x_batch, min_x_batch])
-        #print('shape of stand_params:', standardisation_params.shape)
-        scaled_x_batch = (x_batch-min_x_batch)/(max_x_batch-min_x_batch)
-        
-        y_batch = scaled_x_batch
+        print('Batch number: ',batch_no)
 
-        custom_loss_calls.assign_add(1) # adds one to the counter for how many times this function is entered/ which batch we are on
-        # Train on the batch
-        loss = model.train_on_batch(scaled_x_batch, y_batch, reset_metrics=True)
-        L_batch = model.predict_on_batch(scaled_x_batch)
-        #max_values = np.maximum(max_values, np.max(L_batch, axis=0))
-        #min_values = np.minimum(min_values, np.min(L_batch, axis=0))
+        # Define inputs X (and targets Y)
+        x_batch = scaled_inputs[batch_idx:batch_idx+batch_size] # generates 1 batch of size N as an input to the model
+        y_batch = x_batch # model loss func doesn't use targets, so use dummy target values
 
+        # Train on batch 
+        loss = model.train_on_batch(x=x_batch, y=y_batch, reset_metrics=True)
+        
+        # Update loss of entire Epoch
         total_loss += loss
 
+        # Update loss / batch counter
+        custom_loss_calls.assign_add(1)
+    
+    # Calculate average loss per batch 
     average_loss = tf.sqrt(tf.square(total_loss)/ batches_per_epoch)
-    losses.append(average_loss)
+    print('Avg loss per batch: ', average_loss.numpy())
+    loss_list.append(average_loss) # track loss of all epochs for plotting
 
 
-#getting output for last batch only (last time step)
-L_finalT = tf.transpose(L_batch,perm=[0,2,1])
+
+
+#%%########################################################################################################
+# Plot training loss history
+    
+N_epochs_range  = np.arange(1, N_epochs + 1)
+plt.plot(N_epochs_range, loss_list, label='Avg Batch Training Loss per Epoch')
+#plt.plot  ( N_epochs, history.history['val_loss' ], label = 'Validation')
+#plt.title ('Training and validation loss')
+#plt.legend();
+
+
+
+
+#%%########################################################################################################
+# Getting output for last batch only (last time step)
+
+'''    
+    
+L_finalT = tf.transpose(stacked_L,perm=[0,2,1])
 
 ############################################################################################################################        
 # Calculate C
-C_final = tf.matmul(L_batch, L_finalT) # size: (N,3,3) 
+C_final = tf.matmul(stacked_L, L_finalT) # size: (N,3,3) 
 
-Exx = x_batch[:,0]
-Eyy = x_batch[:,1]
-Exy = x_batch[:,2]
 
-epsilon = tf.stack([Exx, Eyy, Exy], axis=1)
+print('final C', C_final)
+print('final C shape', C_final.shape)
+
+
+
+epsilon = tf.stack([Exx_data, Eyy_data, Exy_data], axis=1)
 epsilon = tf.reshape(epsilon , (epsilon.shape[0],3,1)) #sise:
 
 
 stress = tf.matmul(C_final, epsilon)
-print('stress shape:', stress.shape)
+#print('stress shape:', stress.shape)
 
 reshaped_tensor = tf.reshape(stress, (stress.shape[0], -1))
 
@@ -268,24 +246,10 @@ csv_file_path = 'output_stress.csv'
 # Use NumPy's savetxt function to save the array to a CSV file
 np.savetxt(csv_file_path, numpy_array, delimiter=',')
 
-print('stress at final batch:', stress)
-print('stress type:', type(stress))
-
-print('final L:', L_batch)
-print('final loss:', average_loss)
-
 # After training loop, you can use max_values and min_values
 #print("Max values:", max_values)
 #print("Min values:", min_values)
 
 
 #loss = model.evaluate(inputs, inputs, verbose=0)
-
-# Plot history
-epochs_range  = np.arange(1, epochs + 1)
-plt.plot(epochs_range, losses, label='Training Loss')
-#plt.plot  ( epochs, history.history['val_loss' ], label = 'Validation')
-#plt.title ('Training and validation loss')
-#plt.legend();
-
-#%%
+'''
